85124f5c58c7e2d612fdc269a3d8fe3f3be40832
Saksham Aurora <49725699+sakshm-aurora@users.noreply.github.com>
2020-05-12 02:27:47 +0530

    Add files via upload

---
+++ request.py
@@ -0,0 +1,6 @@
+import requests
+
+url = 'http://localhost:5000/predict_api'
+r = requests.post(url,json={'petal_length':5.2, 'petal_width':3.5, 'sepal_length':2.4 , sepal_width:1.2 })
+
+print(r.json())
